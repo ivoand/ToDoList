d29b92eae874e8efaa787770b7bdb5ec3fbf19f8
Ivo <60362965+chanderslv@users.noreply.github.com>
2021-01-17 14:04:32 +0200

    event with edit, counting and first steps with localStorige

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let template = document.querySelector(".template");
+let count = 0;
 
 document
     .querySelector(".new-task")
@@ -8,28 +9,76 @@ document
 
         if (textarea.value !== "") {
             let task = template.cloneNode(true);
+            count++;
+            task.setAttribute("data-order", count);
             task.classList.remove("template");
             task.querySelector("pre").textContent = textarea.value;
+            localStorage.setItem("task[" + count + "]", textarea.value);
+            localStorage.setItem("task_count", count);
 
             task.addEventListener("click", function () {
-                this.classList.toggle("done");
+                if (!this.classList.contains("editable")) {
+                    this.classList.toggle("done");
+
+                }
+
             });
+
+            // Option pogas click
 
             task.querySelector(".option").addEventListener("click", function (event) {
                 event.stopPropagation();
                 task.querySelector(".options").classList.toggle("active");
+
             });
+
+            // Edit pogas click
 
             task.querySelector(".edit").addEventListener("click", function (e) {
                 e.stopPropagation();
-                console.log("edit");
-            });
-            task.querySelector(".remove").addEventListener("click", function (e) {
-                e.stopPropagation();
-                console.log("remove");
+                if (!task.classList.contains("done")) {
+                    task.querySelector("pre").setAttribute("contenteditable", true);
+                    task.classList.add("editable");
+                    task.querySelector(".options").classList.remove("active");
+                }
+
             });
 
+            // SAVE pogas click
+
+            task.querySelector(".save").addEventListener("click", function (e) {
+                e.stopPropagation();
+
+                task.classList.remove("editable");
+                task.querySelector("pre").removeAttribute("contenteditable");
+            });
+
+            // Remove pogas click
+
+            task.querySelector(".remove").addEventListener("click", function (e) {
+                e.stopPropagation();
+                task.remove();
+            });
+
+            // UP pogas click
+
+            task.querySelector(".up").addEventListener("click", function (e) {
+                e.stopPropagation();
+            });
+
+            // DOWN pogas click
+
+            task.querySelector(".down").addEventListener("click", function (e) {
+                e.stopPropagation();
+            });
+
+
+
+
+
+
             document.querySelector(".task-list").append(task);
+
 
             textarea.value = "";
         }
